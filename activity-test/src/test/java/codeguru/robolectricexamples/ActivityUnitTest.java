f13b4b3f9717194282801da102e29d528bedb429
Code Apprentice <codeguru42@gmail.com>
2017-05-30 12:28:53 -0600

    Add test for TextView

--- activity-test/src/test/java/codeguru/robolectricexamples/ActivityUnitTest.java
+++ activity-test/src/test/java/codeguru/robolectricexamples/ActivityUnitTest.java
@@ -1,6 +1,7 @@
 package codeguru.robolectricexamples;
 
 import android.app.Activity;
+import android.widget.TextView;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +9,7 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
@@ -17,5 +19,13 @@ public class ActivityUnitTest {
     public void testActivity() {
         Activity activity = Robolectric.setupActivity(MainActivity.class);
         assertNotNull(activity);
+    }
+
+    @Test
+    public void testTextView() throws Exception {
+        Activity activity = Robolectric.setupActivity(MainActivity.class);
+        TextView hello = (TextView) activity.findViewById(R.id.hello_text);
+        assertNotNull(hello);
+        assertEquals("Hello, World", hello.getText().toString());
     }
 }
