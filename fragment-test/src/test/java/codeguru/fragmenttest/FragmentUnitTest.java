0fe5edbf0cc89e6637e735f4dcefef1acbe8e2fd
Code Apprentice <codeguru42@gmail.com>
2017-06-05 10:30:02 -0600

    Stub for FragmentUnitTest

---
+++ fragment-test/src/test/java/codeguru/fragmenttest/FragmentUnitTest.java
@@ -0,0 +1,15 @@
+package codeguru.fragmenttest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class FragmentUnitTest {
+    @Test
+    public void testFragment() throws Exception {
+
+    }
+}
