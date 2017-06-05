655ae4957140060637a9be435c024a844b8002b6
Code Apprentice <codeguru42@gmail.com>
2017-06-05 10:35:38 -0600

    Implement testFragment()

--- fragment-test/src/test/java/codeguru/fragmenttest/FragmentUnitTest.java
+++ fragment-test/src/test/java/codeguru/fragmenttest/FragmentUnitTest.java
@@ -1,15 +1,22 @@
 package codeguru.fragmenttest;
 
+import android.support.v4.app.Fragment;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class FragmentUnitTest {
     @Test
     public void testFragment() throws Exception {
-
+        Fragment fragment = new MainFragment();
+        startFragment(fragment);
+        assertNotNull(fragment);
     }
 }
